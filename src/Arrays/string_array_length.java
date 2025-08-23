65c313480396cb558d61dbdeb0ce73b8d574cc9e
ShubhamJha831 <shubhamjhamailme@gmail.com>
2025-08-23 09:20:19 +0530

    code

---
+++ src/Arrays/string_array_length.java
@@ -0,0 +1,12 @@
+package Arrays;
+
+public class string_array_length {
+        public static void main(String[] args) {
+            String[] strarr = new String[4];
+            strarr[0] = "My String";
+
+            String[] newstrarr = {"first", "second", "third"};
+            System.out.println(newstrarr.length);
+    }
+
+}
