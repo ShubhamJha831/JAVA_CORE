90195e316c7c73b82016465b2d6877e1b48e0450
ShubhamJha831 <shubhamjhamailme@gmail.com>
2025-08-24 14:03:29 +0530

    code

---
+++ src/challanges/find_sum_and_average.java
@@ -0,0 +1,10 @@
+package challanges;
+import java.util.Arrays;
+
+class find_sum_and_average {
+    public static void main(String[] args) {
+
+        int[] numArray = array_utility.inputArray();
+        System.out.println(Arrays.toString(numArray));
+    }
+}
