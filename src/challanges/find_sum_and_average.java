e35c3e5962d082a644f99ec7b7076cb149d91de1
ShubhamJha831 <shubhamjhamailme@gmail.com>
2025-08-25 21:18:29 +0530

    code

--- src/challanges/find_sum_and_average.java
+++ src/challanges/find_sum_and_average.java
@@ -3,8 +3,7 @@ import java.util.Arrays;
 
 class find_sum_and_average {
     public static void main(String[] args) {
+        System.out.println("shubham madharchod hai ek number ka ");
 
-        int[] numArray = array_utility.inputArray();
-        System.out.println(Arrays.toString(numArray));
     }
 }
