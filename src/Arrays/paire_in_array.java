4efefdd901a2ebcc82293231051e5f20df07d80a
ShubhamJha831 <shubhamjhamailme@gmail.com>
2025-08-25 22:04:58 +0530

    code

---
+++ src/Arrays/paire_in_array.java
@@ -0,0 +1,20 @@
+package Arrays;
+
+public class paire_in_array
+{
+    public static void printpairs(int number[]) {
+        for (int i = 0; i < number.length; i++){
+            int  curr = number[i];
+            for(int j = i+1; j<number.length; j++){
+                System.out.println("(" + curr + "," + number[j] + ") ");
+
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int number[] = {2, 4, 6, 8, 10};
+        printpairs(number);
+    }
+}
--- src/challanges/find_sum_and_average.java
+++
@@ -1,9 +0,0 @@
-package challanges;
-import java.util.Arrays;
-
-class find_sum_and_average {
-    public static void main(String[] args) {
-        System.out.println("shubham madharchod hai ek number ka ");
-
-    }
-}
