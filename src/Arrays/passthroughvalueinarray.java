df8f8b19240f8b235a506deb90e3d5ad596561b9
ShubhamJha831 <shubhamjhamailme@gmail.com>
2025-08-27 21:49:03 +0530

    code

---
+++ src/Arrays/passthroughvalueinarray.java
@@ -0,0 +1,19 @@
+
+package Arrays;
+public class passthroughvalueinarray {
+    public static void update(int marks[]) {
+        for(int i = 0; i<marks.length; i++){
+            marks[i] = marks[i]+1;
+        }
+    }
+
+    public static void main(String[] args) {
+        int marks[] = {36, 52, 54};
+        update(marks);
+
+        for(int i=0; i<marks.length; i++){
+            System.out.println(marks[i]+" ");
+        }
+        System.out.println();
+    }
+}
