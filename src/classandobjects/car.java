2075d31d6ac5a8ede1840e1795cc13316ed9b2dd
ShubhamJha831 <shubhamjhamailme@gmail.com>
2025-08-28 15:36:44 +0530

    code

--- src/classandobjects/car.java
+++ src/classandobjects/car.java
@@ -1,5 +1,24 @@
 package classandobjects;
 
 public class car {
-    car mycar = new car();
+    int noofWheels;
+    String color;
+    float maxSpeed;
+    float currentFuelINLiters;
+    int noofSeat;
+
+    public void drive(){
+        System.out.println("car is driving");
+        currentFuelINLiters--;
+    }
+
+    public void addfuel(float fuel){
+        currentFuelINLiters += fuel;
+    }
+    public float getCurrentFuelevel(){
+        return currentFuelINLiters;
+    }
+
+
+
 }
