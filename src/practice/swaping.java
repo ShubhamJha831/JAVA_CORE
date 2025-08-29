8ecb36ebb91b5d262d2cf531bbf1c2890585f1f9
ShubhamJha831 <shubhamjhamailme@gmail.com>
2025-08-29 21:20:13 +0530

    code

---
+++ src/practice/swaping.java
@@ -0,0 +1,14 @@
+package practice;
+
+public class swaping {
+    public static void swap(int a, int b) {
+        System.out.println("a = " + b);
+        System.out.println("b = " + a);
+    }
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 7;
+        swap(a, b);
+    }
+}
---
+++ src/practice/tax_calculator.java
@@ -0,0 +1,22 @@
+package practice;
+
+import java.util.Scanner;
+
+import java.util.Scanner;
+
+public class tax_calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Float income = sc.nextFloat();
+        float tax;
+        if (income < 500000.0F) {
+            tax = 0.0F;
+        } else if (income >= 500000.0F && income < 1000000.0F) {
+            tax = (float)((double)income * 0.2);
+        } else {
+            tax = (float)((double)income * 0.3);
+        }
+
+        System.out.println("your tax is " + tax);
+    }
+}
