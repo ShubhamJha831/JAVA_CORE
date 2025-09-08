a77c752f48902ec6938a03d79c2628d2067c6181
ShubhamJha831 <shubhamjhamailme@gmail.com>
2025-09-08 09:42:49 +0530

    code

---
+++ src/CHALLANGES/Challange_no_57.java
@@ -0,0 +1,35 @@
+package CHALLANGES;
+
+import java.util.Scanner;
+
+public class Challange_no_57 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to Day of the week detector\n");
+        System.out.println("Enter your Day  in number: ");
+        int day = input.nextInt();
+        //oldSwitch(day);
+        newSwitch(day);
+
+    }
+
+    public static void newSwitch(int day) {
+        String monthstr = switch (day) {
+            case 1 -> "January";
+            case 2 -> "February";
+            case 3 -> "March";
+            case 4 -> "April";
+            case 5 -> "May";
+            case 6 -> "June";
+            case 7 -> "July";
+            case 8 -> "August";
+            case 9 -> "September";
+            case 10 -> "October";
+            case 11 -> "November";
+            case 12 -> "December";
+            default -> "Invalid";
+        };
+        System.out.println(monthstr);
+
+    }
+}
