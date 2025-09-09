90fab218814d186d17a0df870d284f0006a3cec8
ShubhamJha831 <shubhamjhamailme@gmail.com>
2025-09-09 18:33:58 +0530

    code

---
+++ src/CHALLANGES/Challange_no71.java
@@ -0,0 +1,9 @@
+package CHALLANGES;
+class Challange_no71 {
+    public static void main(String[] args) {
+        String FirstName = "shubham";
+        String lastName = "jha";
+        String fullName = FirstName.concat(" ").concat(lastName);
+        System.out.println(fullName.toUpperCase());
+    }
+}
