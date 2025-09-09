d0fcdeeb5dc30943a16a74df2014be3931bc8df9
ShubhamJha831 <shubhamjhamailme@gmail.com>
2025-09-09 19:30:22 +0530

    code

---
+++ src/CHALLANGES/76 CarFinal.java
@@ -0,0 +1,15 @@
+package CHALLANGES;
+
+class CarFinal {
+    final int noOfWeels;
+
+    final String model;
+
+    final String engineInLiters;
+
+    public CarFinal(int noOfWeels, String model, String engineInLiters) {
+        this.noOfWeels = noOfWeels;
+        this.model = model;
+        this.engineInLiters = engineInLiters;
+    }
+}
