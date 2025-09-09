2c831603db10bb39abbd4ce84cf748e88a192090
ShubhamJha831 <shubhamjhamailme@gmail.com>
2025-09-09 12:03:06 +0530

    code

---
+++ src/control_statement/StringFormat.java
@@ -0,0 +1,10 @@
+package control_statement;
+
+public class StringFormat {
+    public static void main(String[] args) {
+        String name = "Sandeep";
+        int marks = 45;
+        System.out.println("Hello " + name + " Your marks are: " + 45);
+        System.out.printf("Hello %s, Your marks are: %d", name, marks);
+    }
+}
