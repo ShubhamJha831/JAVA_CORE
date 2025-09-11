ba1d0815b1fa6580a0da947aff289a945695e157
ShubhamJha831 <shubhamjhamailme@gmail.com>
2025-09-11 20:35:51 +0530

    code

---
+++ src/Challange_no_77/geometry/Circle.java
@@ -0,0 +1,10 @@
+package Challange_no_77.geometry;
+
+public class Circle {
+    public double radius;
+
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+}
---
+++ src/Challange_no_77/util/Calculator.java
@@ -0,0 +1,21 @@
+package Challange_no_77.util;
+
+import Challange_no_77.geometry.Circle;
+import Challange_no_77.geometry.Rectangle;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+
+        Circle cir = new Circle(5.5);
+
+        Rectangle rect = new Rectangle(10, 5);
+
+        double cirArea = Math.PI * Math.pow(cir.radius, 2);
+        double  rectArea = rect.breadth * rect.length;
+
+        System.out.printf("Area of the circle is: %s, area of the rectangle is: %s", cirArea, rectArea);
+
+    }
+
+}
