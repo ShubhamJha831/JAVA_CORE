a660b2d6c9d37627efc96f317073772e61bbd79c
ShubhamJha831 <shubhamjhamailme@gmail.com>
2025-09-16 17:54:13 +0530

    code

---
+++ src/Abstractionandpolymorphism/Car.java
@@ -0,0 +1,16 @@
+package Abstractionandpolymorphism;
+
+public class Car extends Vehicle{
+
+    private int noOfDoors;
+
+
+    public Car(){
+        super(4);
+    }
+
+    @Override
+    public void makeStartSound() {
+
+    }
+}
---
+++ src/Abstractionandpolymorphism/TestAbstraction.java
@@ -0,0 +1,10 @@
+package Abstractionandpolymorphism;
+
+public class TestAbstraction{
+
+    public static void main(String[] args) {
+        //Vehicle Veh = new Vehicle(2);
+        Car car = new Car();
+        car.commute();
+    }
+}
