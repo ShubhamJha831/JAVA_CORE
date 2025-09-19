d8b2273d8165ed9f9ba017affbcb5d444873ad00
ShubhamJha831 <shubhamjhamailme@gmail.com>
2025-09-19 19:16:36 +0530

    code

--- src/Exception_and_file_handling/Exception/Calculator.java
+++ src/Exception_and_file_handling/Exception/Calculator.java
@@ -4,6 +4,20 @@ import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) {
+        a();
+
+    }
+
+    private static void a(){
+        b();
+    }
+    private static void b(){
+        c();
+    }
+    private static void c(){
+        d();
+    }
+    private static void d(){
         Scanner input = new Scanner(System.in);
 
         System.out.println("Welcome to division Calculator");
@@ -14,7 +28,7 @@ public class Calculator {
         int second = input.nextInt();
         try {
             int[] a = new int[5];
-            //System.out.printf("Result is %d", a[6]);
+            System.out.printf("Result is %d", a[6]);
             a[6] = first / second;
             System.out.printf("Result is %d", a[6]);
             int result = first / second;
@@ -24,7 +38,7 @@ public class Calculator {
             System.out.printf("%s, enter valid values", wrong.getMessage());
         }catch (Throwable th){
             System.out.println("Something went wrong");
+            throw th;
         }
-
     }
 }
