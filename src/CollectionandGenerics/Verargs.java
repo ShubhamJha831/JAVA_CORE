37b6de58c461ca13dd33c63a119076a57d01aa3e
ShubhamJha831 <shubhamjhamailme@gmail.com>
2025-10-01 17:41:05 +0530

    code

---
+++ src/CollectionandGenerics/Verargs.java
@@ -0,0 +1,17 @@
+package CollectionandGenerics;
+
+import java.sql.SQLOutput;
+
+public class Verargs{
+    public static void main(String[] args) {
+        System.out.println(sum(1,1,1,1,1 ,1,1,1,1));
+    }
+
+    public static int sum(int first, int second, int... a){
+        int sum = 0;
+        for (int i : a) {
+            sum += i;
+        }
+        return sum;
+    }
+}
